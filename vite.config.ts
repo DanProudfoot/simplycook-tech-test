fb73174ae763cf40f5f627313a9cc0068c07eb38
Dan Proudfoot <dnprdft@gmail.com>
2024-12-01 14:00:58 +0000

    Clear out bootstrap files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { pigment } from "@pigment-css/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), pigment({})],
+	plugins: [react(), pigment({}), tsconfigPaths()],
 });
