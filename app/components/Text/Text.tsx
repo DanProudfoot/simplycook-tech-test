dd9382ee6eef78d2f0408817c03d0ddee2495a0d
Dan Proudfoot <dnprdft@gmail.com>
2024-12-02 23:08:22 +0000

    Text styles

---
+++ app/components/Text/Text.tsx
@@ -0,0 +1,19 @@
+import { styled } from "@pigment-css/react";
+
+export const BodySBold = styled.h3`
+  color: var(--colour-text-strong);
+
+  font-weight: var(--font-weight-bold);
+  font-size: var(--font-size-body-s);
+  line-height: var(--line-height-body-s);
+  letter-spacing: var(--letter-spacing-body-s);
+`;
+
+export const DetailText = styled.p`
+  color: var(--colour-text-detail);
+
+  font-weight: var(--font-weight-regular);
+  font-size: var(--font-size-detail);
+  line-height: var(--line-height-detail);
+  letter-spacing: var(--letter-spacing-detail);
+`;
